09f07699f21f7f75cf00e4852728527da614264d
CN138 <cn138@CN0138.local>
2019-09-20 17:22:15 +0100

    update

--- public/index.js
+++ public/index.js
@@ -7,11 +7,15 @@ const pic = document.getElementById('image');
 
 button.addEventListener('click', async () => {
     place.innerHTML="<img src='./loading1.gif' />"
+    summary.textContent=" ";
+    temp.textContent =" ";
+    pic.innerHTML = " ";
+
 
     let response = await fetch(`http://localhost:3006/weather?location=${input.value}`);
     let data = await response.json();
     summary.textContent = data.summary;
-    pic.innerHTML = data.icon;
+    pic.innerHTML = data.icons;
     place.textContent = data.place;
     temp.textContent = `${data.temp}°C`
 })
